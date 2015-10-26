4a02c95909304635ef2e84fc2fc26abd6fe39396
Cassidy Harpster <cassidy.j.harpster@gmail.com>
2015-10-26 15:57:10 -0400

    Added functionality to submitting the cards as arrays that print out one at a time

--- script.js
+++ script.js
@@ -18,11 +18,24 @@ $('#submitButton').click(function(){
     a.innerHTML=answerArray.join('<br/>');
 });
 
+var i = 0;
+var j = 0;
+$('#submitButton').click(function(){
+  $('#hints').text('Press the button to begin!');
+  $('#answers').text('This is the answer side. Start here to play "Jeopardy" style!');
+});
+
+$('.nextQuestion').click(function(){
+  $('#hints').text(hintArray[i++]);
+  $('#answers').text(answerArray[j++]);
+});
+
 $('#startGame').click(function(){
   $('#dataTable').toggle('slow');
   $('h3').toggle('slow');
   $('#submitCard').toggle('slow');
   $('.stage').toggle('slow');
+  $('.nextQuestion').toggle('slow');
   $(this).html($(this).html() == 'Click here to start' ? 'Add more cards' : 'Click here to start');
 })
 
