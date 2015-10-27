b3cd2ca33c77799b236cf26991f923d6ca7f1e15
Cassidy Harpster <cassidy.j.harpster@gmail.com>
2015-10-27 10:12:25 -0400

    Changed the fields holding the input hints and answers from 2 separate arrays to 1 object containing 2 arrays

--- script.js
+++ script.js
@@ -6,39 +6,70 @@ $('#submitCard').submit(function(e) {
     e.preventDefault();
 });
 
-var hintArray = [];
-var answerArray = [];
+// This is the JS object for the cards
+var cards = {
+  hintsObject: [],
+  answersObject: [],
+}
 
+// This takes the input and puts it in the cards array
 $('#submitButton').click(function(){
     var h = document.getElementById('hintTable');
-    hintArray.push(document.getElementById('hint').value);
-    h.innerHTML=hintArray.join('<br/>');
+    cards.hintsObject.push(document.getElementById('hint').value);
+    h.innerHTML=cards.hintsObject.join('<br/>');
     var a = document.getElementById('answerTable');
-    answerArray.push(document.getElementById('answer').value);
-    a.innerHTML=answerArray.join('<br/>');
+    cards.answersObject.push(document.getElementById('answer').value);
+    a.innerHTML=cards.answersObject.join('<br/>');
+    submitCard.reset();
 });
 
 var i = 0;
 var j = 0;
-$('#submitButton').click(function(){
-  $('#hints').text('Press the button to begin!');
-  $('#answers').text('This is the answer side. Start here to play "Jeopardy" style!');
-});
 
+// This moves from one item in the array to the next or previous
 $('.nextQuestion').click(function(){
-  $('#hints').text(hintArray[i++]);
-  $('#answers').text(answerArray[j++]);
+    i++;
+    displayCard(i);
 });
 
+$('.prevQuestion').click(function(){
+    i--;
+    displayCard(i);
+});
+
+// This prints the text to the cards on the screen
+function displayCard(i) {
+    $('#hints').empty();
+    $('#hints').append(cards.hintsObject[i]);
+    $('#answers').empty();
+    $('#answers').append(cards.answersObject[i]);
+
+    if(i == 0)
+        $('.prevQuestion').hide();
+    else
+        $('.prevQuestion').show();
+
+    if(i == cards.hintsObject.length-1)
+        $('.nextQuestion').hide();
+    else
+        $('.nextQuestion').show();
+};
+
+// This swipes all the starting screen information in and out in a nifty fashion
 $('#startGame').click(function(){
+  // cards.hintsObject.sort(function(a, b){
+  //   return 0.5-Math.random();
+  // });
   $('#dataTable').toggle('slow');
   $('h3').toggle('slow');
   $('#submitCard').toggle('slow');
   $('.stage').toggle('slow');
   $('.nextQuestion').toggle('slow');
+  $('.prevQuestion').toggle('slow');
   $(this).html($(this).html() == 'Click here to start' ? 'Add more cards' : 'Click here to start');
 })
 
+// This links to the fancy animation webkit you can see in the CSS to flip the cards
 $('.flashcard').on('click', function() {
     $('.flashcard').toggleClass('flipped');
   });
